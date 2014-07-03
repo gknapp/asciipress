76d1256ebc272825bdd2d78a962579bebd1867c4
Greg Knapp <virtual.greg@gmail.com>
2014-07-03 02:09:59 +0100

    Pass article dir as argument

--- publish.sh
+++ publish.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+	echo "Missing articles directory path"
+	exit 1
+fi
+
+SOURCE=$1
 DIR="$(cd "$( dirname "$0" )" && pwd)"
-SITE="$DIR/../gknapp.github.io"
-SOURCE="$SITE/articles"
 DEST="$DIR/xml"
+
+cd $DIR
 
 echo "Reading articles from: ${SOURCE/$DIR\//}"
 
@@ -11,14 +17,13 @@ for ASCFILE in $(ls $SOURCE/*.asc)
 do
 	XMLFILE=${ASCFILE/.asc/.xml}
 	XMLFILE=${XMLFILE/$SOURCE/$DEST}
-	echo "asciidoc --backend=docbook -a disable-javascript -o $XMLFILE $ASCFILE"
+	# echo "asciidoc --backend=docbook -a disable-javascript -o $XMLFILE $ASCFILE"
 	asciidoc --backend=docbook -a disable-javascript -o $XMLFILE $ASCFILE
 
 	HTMLFILE=${XMLFILE/.xml/.html}
-	HTMLFILE=${HTMLFILE/$DEST/$SITE}
-	echo "xsltproc $DIR/index.xsl $XMLFILE > $HTMLFILE"
-	echo ""
+	HTMLFILE=${HTMLFILE/$DEST/$SOURCE\/..}
+	# echo ""
+	# echo "xsltproc $DIR/index.xsl $XMLFILE > $HTMLFILE"
 	xsltproc "$DIR/index.xsl" $XMLFILE > $HTMLFILE
 	echo "Published article: $HTMLFILE" 
 done
-
