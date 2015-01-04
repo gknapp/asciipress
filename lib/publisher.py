30d0168756f9719085c8f73bd4c25b656bd1e143
Greg Knapp <virtual.greg@gmail.com>
2015-01-04 00:59:57 +0000

    Generate blog posts

--- lib/publisher.py
+++ lib/publisher.py
@@ -1,9 +1,11 @@
 import multiprocessing as mp
-import path
+import path, subprocess
 
 class Publisher:
     def __init__(self, console):
         self.console = console
+        self.converted = 0
+        self.errors = 0
 
     def to_html(self, files, xslt):
         """Convert asciidoc files to HTML in parallel"""
@@ -19,17 +21,34 @@ class Publisher:
 
         p.close()
         p.join()
+        summary = (self.converted, self.errors)
+        self.console.echo(
+            "Generated %d HTML file(s) with %d error(s)" % summary
+        )
 
     def convert(self, work, xslt):
         source, target = work
-        self.console.echo(
-            "Converting %s using %s" % (path.fname(source), xslt)
-        )
+        xslt = "%s/xsl/%s" % (path.dir(source), xslt)
+        exitcode = self.asc_to_html(source, target, xslt)
+        work.append(exitcode)
         return work
 
     def complete(self, work):
-        source, target = work
-        self.console.echo("Converted " + path.fname(source))
+        source, target, code = work
+        if code == 0:
+            self.converted += 1
+        else:
+            self.errors += 1
+            self.console.echo("Error converting" + path.fname(source))
 
     def error(self, ex):
+        self.errors += 1
         self.console.echo(str(ex))
+
+    def asc_to_html(self, ascfile, htmlfile, xslt):
+        asccmd = "asciidoc --backend=docbook -a disable-javascript -o - " + ascfile
+        xslcmd = "xsltproc -o %s %s -" % (htmlfile, xslt)
+        cmd = "%s | %s" % (asccmd, xslcmd)
+        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        proc.communicate()
+        return proc.poll()
